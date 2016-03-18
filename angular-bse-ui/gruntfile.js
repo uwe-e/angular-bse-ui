27d706f3453dc0ffbbf5703ea3fb4e53515d87dc
uwe-e <git.bsetunes@outlook.com>
2016-03-18 14:19:27 +0100

    commit 18032016

--- angular-bse-ui/gruntfile.js
+++ angular-bse-ui/gruntfile.js
@@ -3,7 +3,146 @@ This file in the main entry point for defining grunt tasks and using grunt plugi
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Project configuration.
+    grunt.util.linefeed = '\n';
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        modules: [],//to be filled in by build task
+        dist: 'dist',
+        filename: 'bse.ui',
+        meta: {
+            modules: 'angular.module("bse.ui", [<%= srcModules %>]);'
+        },
+        concat: {
+            dist: {
+                src: [], //src filled in by build task
+                dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.js'
+            }
+        },
+        uglify: {
+            dist: {
+                src: ['<%= concat.dist.dest %>'],
+                dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.min.js'
+            }
+        }
+    });
+
+    //Common ui.bootstrap module containing all modules for src and templates
+    //findModule: Adds a given module to config
+    var foundModules = {};
+    function findModule(name) {
+        if (foundModules[name]) {
+            return;
+        }
+        foundModules[name] = true;
+        function breakup(text, separator) {
+            return text.replace(/[A-Z]/g, function (match) {
+                return separator + match;
+            });
+        }
+        function ucwords(text) {
+            return text.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+                return $1.toUpperCase();
+            });
+        }
+        function enquote(str) {
+            return '"${str}"';
+        }
+        function enquoteUibDir(str) {
+            return enquote('bse/${str}');
+        }
+
+        var module = {
+            name: name,
+            moduleName: enquote('bse.ui.${name}'),
+            displayName: ucwords(breakup(name, ' ')),
+            srcFiles: grunt.file.expand(['src/${name}/*.js', '!src/${name}/index.js']),
+            cssFiles: grunt.file.expand('src/${name}/*.css'),
+            tplFiles: grunt.file.expand('template/${name}/*.html'),
+            //tpljsFiles: grunt.file.expand('template/${name}/*.html.js'),
+            tplModules: grunt.file.expand('template/${name}/*.html').map(enquoteUibDir),
+            dependencies: dependenciesForModule(name),
+            //docs: {
+            //    md: grunt.file.expand('src/${name}/docs/*.md')
+            //      .map(grunt.file.read).map((str) => marked(str)).join('\n'),
+            //    js: grunt.file.expand('src/${name}/docs/*.js')
+            //      .map(grunt.file.read).join('\n'),
+            //    html: grunt.file.expand('src/${name}/docs/*.html')
+            //      .map(grunt.file.read).join('\n')
+            //}
+        };
+        var styles = {
+            css: [],
+            js: []
+        };
+        //module.cssFiles.forEach(processCSS.bind(null, module.name, styles, true));
+        //if (styles.css.length) {
+        //    module.css = styles.css.join('\n');
+        //    module.cssJs = styles.js.join('\n');
+        //}
+
+        module.dependencies.forEach(findModule);
+        grunt.config('modules', grunt.config('modules').concat(module));
+    }
+
+    function dependenciesForModule(name) {
+        var deps = [];
+        grunt.file.expand('src/' + name + '/*.js')
+        .map(grunt.file.read)
+        .forEach(function (contents) {
+            //Strategy: find where module is declared,
+            //and from there get everything inside the [] and split them by comma
+            var moduleDeclIndex = contents.indexOf('angular.module(');
+            var depArrayStart = contents.indexOf('[', moduleDeclIndex);
+            var depArrayEnd = contents.indexOf(']', depArrayStart);
+            var dependencies = contents.substring(depArrayStart + 1, depArrayEnd);
+            dependencies.split(',').forEach(function (dep) {
+                if (dep.indexOf('bse.ui.') > -1) {
+                    var depName = dep.trim().replace('bse.ui.', '').replace(/['"]/g, '');
+                    if (deps.indexOf(depName) < 0) {
+                        deps.push(depName);
+                        //Get dependencies for this new dependency
+                        deps = deps.concat(dependenciesForModule(depName));
+                    }
+                }
+            });
+        });
+        return deps;
+    }
+
+    grunt.registerTask('dist', 'Override dist directory', function () {
+        var dir = this.args[0];
+        if (dir) { grunt.config('dist', dir); }
+    });
+
+    grunt.registerTask('build', 'Create bse.ui build files', function () {
+        var _ = grunt.util._;
+
+        if (this.args.length) {
+            this.args.forEach(findModule);
+            grunt.config('filename', grunt.config('filenamecustom'));
+        } else {
+            grunt.file.expand({
+                filter: 'isDirectory', cwd: '.'
+            }, 'src/*').forEach(function (dir) {
+                findModule(dir.split('/')[1]);
+            });
+        }
+
+        //If arguments define what modules to build, build those. Else, everything
+        var modules = grunt.config('modules');
+        grunt.config('srcModules', _.pluck(modules, 'moduleName'));
+
+
+        var srcFiles = _.pluck(modules, 'srcFiles');
+        //Set the concat task to concatenate the given src modules
+        grunt.config('concat.dist.src', grunt.config('concat.dist.src')
+                     .concat(srcFiles));
+
+        grunt.task.run(['concat', 'uglify']);
+        
     });
 };
---
+++ angular-bse-ui/src/icontabs/icontabs.js
@@ -0,0 +1,189 @@
+﻿angular.module("bse.ui.icontabs", [])
+    .controller('BseIconTabsetController', ['$scope', function ($scope) {
+        var ctrl = this,
+            oldIndex;
+        ctrl.tabs = [];
+
+        ctrl.select = function (index) {
+            if (!destroyed) {
+                var previousIndex = findTabIndex(oldIndex);
+                var previousSelected = ctrl.tabs[previousIndex];
+                if (previousSelected) {
+                    previousSelected.tab.onDeselect();
+                    previousSelected.tab.active = false;
+                }
+
+                var selected = ctrl.tabs[index];
+                if (selected) {
+                    selected.tab.onSelect();
+                    selected.tab.active = true;
+                    ctrl.active = selected.index;
+                    oldIndex = selected.index;
+                } else if (!selected && angular.isNumber(oldIndex)) {
+                    ctrl.active = null;
+                    oldIndex = null;
+                }
+            }
+        };
+
+        ctrl.addTab = function addTab(tab) {
+            ctrl.tabs.push({
+                tab: tab,
+                index: tab.index
+            });
+        };
+
+        ctrl.removeTab = function removeTab(tab) {
+            var index = findTabIndex(tab.index);
+
+            if (tab.index === ctrl.active) {
+                var newActiveTabIndex = index === ctrl.tabs.length - 1 ?
+                  index - 1 : index + 1 % ctrl.tabs.length;
+                ctrl.select(newActiveTabIndex);
+            }
+            ctrl.tabs.splice(index, 1);
+        };
+
+        $scope.$watch('tabset.active', function (val) {
+            if (angular.isNumber(val) && val !== oldIndex) {
+                ctrl.select(findTabIndex(val));
+            }
+        });
+
+        var destroyed;
+        $scope.$on('$destroy', function () {
+            destroyed = true;
+        });
+
+        function findTabIndex(index) {
+            for (var i = 0; i < ctrl.tabs.length; i++) {
+                if (ctrl.tabs[i].index === index) {
+                    return i;
+                }
+            }
+        }
+    }])
+    .directive('BseIconTabset', function () {
+        return {
+            transclude: true,
+            replace: true,
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'bse/template/icontabs/icontabset.html';
+            },
+            controller: 'BseIconTabsetController',
+            restrict: 'EA'
+        };
+    })
+    .directive('BseIconTab', ['$document', function ($document) {
+        return {
+            require: '^BseIconTabset',
+            transclude: true,
+            replace: true,
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'bse/template/icontabs/icontab.html';
+            },
+            restrict: 'EA',
+            controller: function () {
+            },
+            scope: {
+                text: '@',
+                count: '@',
+                icon: '@',
+                orientation: '@',
+                color: '@',
+                onSelect: '&select',    //This callback is called in contentHeadingTransclude
+                //once it inserts the tab's content into the dom
+                onDeselect: '&deselect'
+            },
+            link: function (scope, element, attrs, tabsetCtrl, transclude) {
+                var tabElement = angular.element(element[0]);
+                var paneElement = angular.element(element[0].querySelector('.icon-tab-item-icon-pane'));
+                var textElement = angular.element(element[0].querySelector('.icon-tab-item-text'));
+                var arrowElement = angular.element(element[0].querySelector('.icon-tab-item-arrow'));
+
+                var hasIcon;
+
+                tabElement.addClass('tab-item-orientation-vertical');
+                var color = attrs.color;
+                if (!angular.isDefined(color)) {
+                    color = 'tab-primary';
+                }
+
+                var iconElement = createElement(attrs.icon, 'div');
+                if (iconElement) {
+                    hasIcon = true;
+                    iconElement.addClass('icon-container' + ' ' + color + ' ' + 'glyphicon' + ' ' + attrs.icon);
+                    paneElement.append(iconElement);
+                }
+
+                var countElement = createTextNode(attrs.count, 'span');
+                if (countElement) {
+                    countElement.addClass('icon-tab-item-count');
+                    paneElement.append(countElement);
+
+                    scope.$watch('count', function (count) {
+                        countElement[0].textContent = count;
+                    });
+                }
+
+                var hasText = angular.isDefined(attrs.text);
+                if (hasText) {
+                    var textNode = $document[0].createTextNode(attrs.text);
+                    textElement.append(textNode);
+                }
+                else {
+                    textElement.remove();
+                }
+
+                arrowElement.addClass(color);
+
+                if (hasText && !hasIcon) {
+                    tabElement.addClass('icon-tab-text-only');
+                }
+
+                if (angular.isUndefined(attrs.index)) {
+                    if (tabsetCtrl.tabs && tabsetCtrl.tabs.length) {
+                        scope.index = Math.max.apply(null, tabsetCtrl.tabs.map(function (t) { return t.index; })) + 1;
+                    } else {
+                        scope.index = 0;
+                    }
+                }
+
+                scope.select = function () {
+                    //if (!scope.disabled) {
+                    var index;
+                    for (var i = 0; i < tabsetCtrl.tabs.length; i++) {
+                        if (tabsetCtrl.tabs[i].tab === scope) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    tabsetCtrl.select(index);
+                    //}
+                };
+
+                tabsetCtrl.addTab(scope);
+                scope.$on('$destroy', function () {
+                    tabsetCtrl.removeTab(scope);
+                });
+
+                function createElement(value, tag) {
+                    var element;
+                    if (angular.isDefined(value)) {
+                        element = angular.element($document[0].createElement(tag));
+                    }
+                    return element;
+                }
+                function createTextNode(value, tag) {
+                    var element = createElement(value, tag);
+                    if (element) {
+                        var node = $document[0].createTextNode(value);
+                        if (node) {
+                            element.append(node);
+                        }
+                    }
+                    return element;
+                };
+            }
+        };
+    }]);
