4911496062d2251cf3e7c9509e4de0feec6582c2
Uwe-E <git.bsetunes@outlook.com>
2016-03-22 19:01:12 +0100

    commit after merge

---
+++ angular-bse-ui/src/application/application.js
@@ -0,0 +1,14 @@
+﻿angular.module('bse.ui.application', [])
+    .controller('BseApplicationController', ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+        
+    }])
+    .directive('bseApplicationContent', function () {
+        return {
+            controller: 'BseApplicationController',
+            transclude: true,   // Grab the contents to be used as the heading
+            replace: true,
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'template/application/content.html';
+            },
+        };
+    });
---
+++ angular-bse-ui/src/tiles/tiles.js
@@ -0,0 +1,30 @@
+﻿angular.module('bse.ui.tiles', [])
+    .directive('bseTileContainer', function () {
+        return {
+            transclude: true,   // Grab the contents to be used as the header of a view
+            replace: true,
+            templateUrl: 'template/tiles/tilecontainer.html',
+            restrict: 'EA',
+            scope: {
+                title: '@'
+            },
+            controller: function () {
+                this.setTitle = function (element) {
+                    this.title = element;
+                };
+            }
+        };
+    }).directive('bseTileContainerHeaderTransclude', function () {
+        return {
+            require: '^bseTileContainer',
+            link: function (scope, element, attrs, controller) {
+                scope.$watch(function () { return controller[attrs.mchTileContainerHeaderTransclude]; }, function (title) {
+                    if (title) {
+                        var elem = angular.element(element[0].querySelector('[ui-tile-header-title]'));
+                        elem.html('');
+                        elem.append(title);
+                    }
+                });
+            }
+        };
+    });
