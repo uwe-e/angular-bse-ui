715a29d6d65b2a499815912b5ba2c2af3b5b85b6
uwe-e <git.bsetunes@outlook.com>
2016-06-01 15:36:00 +0200

    scripts and styles changed

---
+++ angular-bse-ui/src/listreport/listreports.js
@@ -0,0 +1,35 @@
+﻿angular.module('bse.ui.listreport', [])
+    .directive('bseListReport', function () {
+        return {
+            transclude: true,   // Grab the contents to be used as the application's viewport.
+            replace: true,
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'template/listreport/listreport.html';
+            }
+        };
+    })
+    .directive('bseListReportToolbar', function () {
+        return {
+            transclude: true,   // Grab the contents to be used as the header of a view
+            replace: true,
+            require: ['^bseListReport', 'bseListReportToolbar'], //requires the parent 'bseView' directive and the local 'bseViewHeader' directive
+            templateUrl: function (element, attrs) {
+                return attrs.templateUrl || 'template/listreport/toolbar.html';
+            },
+            restrict: 'EA',
+            scope: {
+                title: '@'
+            },
+            controller: function () {
+                this.setTitle = function (element) {
+                    this.title = element;
+                };
+                //},
+                //link: function (scope, element, attrs, controllers) {
+                //    var viewCtrl = controllers[0];
+                //    if (viewCtrl) {
+                //        viewCtrl.resizeContent(element);
+                //    }
+            }
+        };
+    });
--- angular-bse-ui/src/views/views.js
+++ angular-bse-ui/src/views/views.js
@@ -19,6 +19,52 @@
             }
         });
         /*
+        * resizes the height of the http://ui-grid.info datagrid.
+        * If the grid is "auto sizable" performed through the "ui-grid-auto-resize" attribute within the grid's div element,
+        * than it's resizable to the available height within the mch-view-content element through using 
+        * the "mch-grid-auto-resize" attribute.
+        *
+        * <div ui-grid="grid" class="grid" ui-grid-save-state ui-grid-auto-resize ui-grid-selection mch-grid-auto-resize></div>
+        *
+        */
+        this.resizeGrid = function (grid) {
+            var view = parents(grid, '.view-content');
+            if (view) {
+                var offsetHeight = 10;
+                //if there is a filterbar on top of the content..
+                var smartBar = view.querySelector('.filterbar');
+                if (smartBar) {
+                    //gets the height of this filterbar
+                    offsetHeight = smartBar.offsetHeight;
+                }
+                //if the grid is located within a list-report panel..
+                var parent = parents(grid, '.list-report');
+                if (parent) {
+                    angular.forEach(parent.children, function (child) {
+                        if (child != grid[0]) {
+                            //gets the height of other elements
+                            offsetHeight += child.offsetHeight;
+                        }
+                    });
+                }
+                grid[0].style.height = view.clientHeight - offsetHeight + 'px';
+            }
+        };
+        /*
+        * Get the ancestors of an element in the current set of matched elements, filtered by a selector.
+        * If there's no ancestor element, the function returns undefined.
+        */
+        function parents(element, selector) {
+            if (element && element[0] !== document) {
+                var el = element[0].querySelector(selector);
+                if (el) {
+                    return el;
+                }
+                return parents(element.parent(), selector);
+            }
+            return;
+        };
+        /*
          * the <bse-view-header/> resp. the <bse-view-footer/> are optional elements. For determination the top and/or bottom position
          * of the <bse-view-conent/> the calculation is performed each time when an element attaches to the view.
          */
@@ -114,6 +160,21 @@
             },
             link: function (scope, element, attrs, headerCtrl, transclude) {
                 headerCtrl.setTitle(transclude(scope, angular.noop));
+            }
+        };
+    })
+    .directive('bseGridAutoResize', function ($timeout, $window) {
+        return {
+            require: '^bseViewContent',
+            restrict: 'A',
+            link: function (scope, element, attrs, applicationCtrl) {
+                $timeout(function () {
+                    applicationCtrl.resizeGrid(element);
+                });
+                //When the window resizes, the grid resizing will be executed again.
+                angular.element($window).bind('resize', function () {
+                    applicationCtrl.resizeGrid(element);
+                });
             }
         };
     });
